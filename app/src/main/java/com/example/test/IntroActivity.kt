9041abd382ea7d111b8f3b2f003e4de3ed27730c
coke0208 <osl89736369@gmail.com>
2024-05-04 17:39:50 +0900

    졸작

--- app/src/main/java/com/example/test/IntroActivity.kt
+++ app/src/main/java/com/example/test/IntroActivity.kt
@@ -3,16 +3,19 @@ package com.example.test
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
+import android.os.Handler
+import android.os.Looper
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        SystemClock.sleep(3000)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
