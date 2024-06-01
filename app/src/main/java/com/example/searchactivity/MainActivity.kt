50e34ff75222baa1537985db4bcd9cf3a11691b8
terryiable <leedm00@naver.com>
2024-06-01 19:18:57 +0900

    5212

---
+++ app/src/main/java/com/example/searchactivity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.searchactivity
+
+import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.searchactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initSearchView()
+    }
+
+    private fun initSearchView() {
+        binding.search.isSubmitButtonEnabled = true
+        binding.search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+}
+
